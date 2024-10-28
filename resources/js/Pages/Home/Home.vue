4a0d33ae02238ae97d3dc7d408ddb687c18e8990
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-10-28 22:08:21 +0200

    top bar independent of auth

--- resources/js/Pages/Home/Home.vue
+++ resources/js/Pages/Home/Home.vue
@@ -1,16 +1,19 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import AmbiauthenicatedLayout from '@/Layouts/AmbiauthenicatedLayout.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 defineProps({
     canLogin: {
         type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
+    }
 });
 </script>
 
 <template>
-    hi!
+    <AmbiauthenicatedLayout :canLogin="canLogin">
+
+    </AmbiauthenicatedLayout>
+
+    huh
 </template>
