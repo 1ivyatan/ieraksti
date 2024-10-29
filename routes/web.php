6f47831ed723e7f8416a6907a6e3efa30a09513d
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-10-29 20:08:39 +0200

    welcome page begone

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/* guests can see routes */
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/home', function () {
     return Inertia::render('Home/Home');
 })->name('home');
-
-//Route::get('/dashboard', function () {
-//    return Inertia::to_route("home");
-//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
