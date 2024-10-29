96e6206f08e5a98795d9208d1379c0d2c643c146
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-10-29 22:31:26 +0200

    learning state managment

---
+++ resources/js/Layouts/MusicPlayer/AudioPlayerBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref, watch, onMounted} from 'vue';
+import {store} from "./Player.js";
+</script>
+
+<template>
+    <div
+        class="fixed bottom-0 left-0 right-0 bg-white border-b border-gray-100"
+    >
+        <div
+            class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+        >
+        <button @click="store.increment()">
+          {{ store.count }}
+        </button>
+        </div>
+    </div>
+</template>
