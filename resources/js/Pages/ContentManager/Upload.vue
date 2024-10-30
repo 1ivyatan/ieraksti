2277d80f21a17cb0883b92ee681a50712af877f4
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-10-30 21:36:16 +0200

    forms fill

--- resources/js/Pages/ContentManager/Upload.vue
+++ resources/js/Pages/ContentManager/Upload.vue
@@ -1,5 +1,9 @@
 <script setup>
 import AmbiauthenicatedLayout from '@/Layouts/AmbiauthenicatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
 import { Head } from '@inertiajs/vue3';
 
 </script>
@@ -12,9 +16,44 @@ import { Head } from '@inertiajs/vue3';
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Profile
+                Upload
             </h2>
         </template>
-        upload!!!!!!
+
+        <section>
+            <form
+                class="mt-6 space-y-6"
+            >
+                <div>
+                    <InputLabel for="title" value="Title" />
+
+                    <TextInput
+                        id="title"
+                        type="text"
+                        class="mt-1 block w-full"
+                        required
+                        autofocus
+                    />
+
+                </div>
+
+                <div>
+                    <InputLabel for="coverfile" value="Audio file" />
+
+                    <input
+                        id="coverfile"
+                        type="file"
+                        class="mt-1 block w-full"
+                        required
+                    />
+
+                </div>
+
+                <div class="flex items-center gap-4">
+                    <PrimaryButton>Upload</PrimaryButton>
+                </div>
+            </form>
+        </section>
+        
     </AmbiauthenicatedLayout>
 </template>
