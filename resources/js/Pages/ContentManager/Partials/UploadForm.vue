c4c78838a2daab86d09a62aed3dd2416e4942ad7
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-10-31 10:28:54 +0200

    damn i had to use useForm

--- resources/js/Pages/ContentManager/Partials/UploadForm.vue
+++ resources/js/Pages/ContentManager/Partials/UploadForm.vue
@@ -5,9 +5,14 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 
+const form = useForm({
+    title: "",
+    cover: ""
+});
+
 const uploadContent = () => {
     form.post("/upload", {
-     // alert("g");
+    //  alert("g");
     });
 };
 
@@ -17,13 +22,8 @@ const uploadContent = () => {
     <form
         class="mt-6 space-y-6"
         enctype="multipart/form-data"
-        method="post"
         @submit.prevent="uploadContent"
     >
-        
-        <div>
-            <InputLabel for="title" value="Title" />
-
             <TextInput
                 id="title"
                 type="text"
@@ -31,23 +31,12 @@ const uploadContent = () => {
                 required
                 autofocus
             />
-
-        </div>
-
-        <div>
-            <InputLabel for="cover" value="Cover" />
-
             <input
                 id="cover"
                 type="file"
                 class="mt-1 block w-full"
                 required
             />
-        </div>
-
-        <div class="flex items-center gap-4">
-            <PrimaryButton
-            >Upload</PrimaryButton>
-        </div>
+            <PrimaryButton>Upload</PrimaryButton>
     </form>
 </template>
