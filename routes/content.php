cdbd4b38099f5298beeb854c6af6ab384b97c0a7
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-01 21:36:31 +0200

    whoops, everyone has to be able to see tracks

--- routes/content.php
+++ routes/content.php
@@ -2,13 +2,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Content\MusicController;
 
-/* guests */
-Route::middleware('guest')->group(function () {
-    /* music */
-    Route::get("/track/{id}", [MusicController::class, "show"])->name('content.music.show');
-});
+/* -- guests */
+/* music */
+Route::get("/track/{id}", [MusicController::class, "show"])->name('content.music.show');
 
-/* only authorized */
+/* -- only authorized */
 Route::middleware('auth')->group(function () {
     Route::get("/upload", [MusicController::class, "create"])->name('content.music.create');;
     Route::post("/upload", [MusicController::class, "store"])->name('content.music.upload');
