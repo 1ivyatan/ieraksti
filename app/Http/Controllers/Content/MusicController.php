c0e3ad633c59a1274e3c01afd434338a665bca03
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-01 12:52:21 +0200

    reorganization

--- app/Http/Controllers/Content/MusicController.php
+++ app/Http/Controllers/Content/MusicController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Content;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
@@ -15,7 +16,7 @@ class MusicController extends Controller
     /* display upload */
     public function upload(Request $request): Response
     {
-        return Inertia::render('ContentManager/Upload');
+        return Inertia::render('Content/Music/Upload');
     }
 
     /* upload */
--- routes/content.php
+++ routes/content.php
@@ -1,9 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MusicController;
+use App\Http\Controllers\Content\MusicController;
 
 Route::middleware('auth')->group(function () {
-    /* upload */
+    /* upload music*/
     Route::get("/upload", [MusicController::class, "upload"])->name('content.create');;
     Route::post("/upload", [MusicController::class, "store"])->name('content.upload');
 });
