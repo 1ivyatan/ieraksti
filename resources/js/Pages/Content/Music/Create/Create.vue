594ca32a5accc18cb83916dff6a1ddba1f55124d
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-02 21:59:11 +0200

    retrieving part of track to edit

--- resources/js/Pages/Content/Music/Create/Create.vue
+++ resources/js/Pages/Content/Music/Create/Create.vue
@@ -1,13 +1,29 @@
 <script setup>
 import AmbiauthenicatedLayout from '@/Layouts/AmbiauthenicatedLayout.vue';
-import UploadForm from './Partials/CreateForm.vue';
-import { Head } from '@inertiajs/vue3';
+import InputError from '@/Components/Input/InputError.vue';
+import InputLabel from '@/Components/Input/InputLabel.vue';
+import PrimaryButton from '@/Components/Button/PrimaryButton.vue';
+import TextInput from '@/Components/Input/TextInput.vue';
+import { Head, useForm } from '@inertiajs/vue3';
 
 const props = defineProps({
     music: {
         type: Object
     }
 });
+
+const form = useForm({
+    title: (props.music) ? props.music.title : "",
+    cover: null
+});
+
+const recordContent = () => {
+    if (music) {
+        
+    } else {
+        form.post("/upload", {});
+    }
+}
 
 const title = (props.music) ? `Edit '${props.music.title}''` : "Upload";
 </script>
@@ -25,7 +41,34 @@ const title = (props.music) ? `Edit '${props.music.title}''` : "Upload";
         </template>
 
         <section>
-            <UploadForm />
+            <form
+                class="mt-6 space-y-6"
+                @submit.prevent="recordContent"
+                enctype="multipart/form-data"
+            >
+                <TextInput
+                    id="title"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.title"
+                    required
+                    autofocus
+
+                />
+
+                <input
+                    id="cover"
+                    type="file"
+                    class="mt-1 block w-full"
+                    @input="form.cover = $event.target.files[0]" 
+                />
+
+                <progress v-if="form.progress" :value="form.progress.percentage" max="100">
+                    {{ form.progress.percentage }}
+                </progress>
+
+                <PrimaryButton>Record</PrimaryButton>
+            </form>
         </section>
         
     </AmbiauthenicatedLayout>
