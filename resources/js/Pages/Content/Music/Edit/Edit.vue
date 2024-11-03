63153a03f9447aadf210cb48f94144b189365777
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-03 22:52:56 +0200

    track title in title

--- resources/js/Pages/Content/Music/Edit/Edit.vue
+++ resources/js/Pages/Content/Music/Edit/Edit.vue
@@ -1,4 +1,6 @@
 <script setup>
+import AmbiauthenicatedLayout from '@/Layouts/AmbiauthenicatedLayout.vue';
+import { Head, useForm } from '@inertiajs/vue3';
 
 defineProps({
     music: {
@@ -9,5 +11,16 @@ defineProps({
 </script>
 
 <template>
-    s
+    <Head :title="'Edit \'' + music.title + '\''" />
+
+    <AmbiauthenicatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Edit "{{ music.title }}"
+            </h2>
+        </template>
+
+    </AmbiauthenicatedLayout>
 </template>
