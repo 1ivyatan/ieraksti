61312d1f0a95521445e0da81ca9a0df27b00cf0c
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-05 21:55:56 +0200

    delete track route add

--- routes/content.php
+++ routes/content.php
@@ -13,4 +13,6 @@ Route::middleware('auth')->group(function () {
 Route::middleware(["auth", "unbanned"])->group(function () {
     Route::get("/upload", [MusicController::class, "create"])->name('content.music.create');
     Route::post("/upload", [MusicController::class, "store"])->name('content.music.upload');
+
+    Route::delete("/track/{id}", [MusicController::class, "destroy"])->name('content.music.destroy');
 });
