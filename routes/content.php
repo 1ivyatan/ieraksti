0104ced44be6d44f4ab6537d536e648c94fada99
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-05 20:53:17 +0200

    looks prettier to me

--- routes/content.php
+++ routes/content.php
@@ -2,18 +2,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Content\Music\MusicController;
 
-/* -- guests */
-/* music  { */
+Route::middleware(['guest', 'auth'])->group(function () {
     Route::get("/track/{id}", [MusicController::class, "show"])->name('content.music.show');
+});
 
-/*} */
-
-/* -- only authorized */
 Route::middleware('auth')->group(function () {
-
-    /* music */
-    Route::get("/upload", [MusicController::class, "create"])->name('content.music.create');
-    Route::post("/upload", [MusicController::class, "store"])->name('content.music.upload');
 
     //Route::get("/track/{id}/edit", [MusicController::class, "edit"])->name('content.music.edit');
     //Route::put("/track/{id}", [MusicController::class, "update"])->name('content.music.update');
@@ -21,4 +14,5 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(["auth", "unbanned"])->group(function () {
     Route::get("/upload", [MusicController::class, "create"])->name('content.music.create');
+    Route::post("/upload", [MusicController::class, "store"])->name('content.music.upload');
 });
