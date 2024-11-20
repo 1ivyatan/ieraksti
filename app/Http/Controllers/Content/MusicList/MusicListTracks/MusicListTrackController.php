24830e2bc1d509713003cf9047a37b9dea37ba0e
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-20 07:14:06 +0200

    musiclisttracks controller add

---
+++ app/Http/Controllers/Content/MusicList/MusicListTracks/MusicListTrackController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Content\MusicList\MusicListTracks;
+
+use App\Http\Controllers\Controller;
+use App\Models\MusicListTracks;
+use Illuminate\Http\Request;
+
+class MusicListTrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MusicListTracks $musicListTracks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MusicListTracks $musicListTracks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MusicListTracks $musicListTracks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MusicListTracks $musicListTracks)
+    {
+        //
+    }
+}
