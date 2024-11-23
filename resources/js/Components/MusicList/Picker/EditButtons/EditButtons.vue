f991ea231f70c92fbe740b4f14be17939530cbcd
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-24 00:07:08 +0200

    item selection

---
+++ resources/js/Components/MusicList/Picker/EditButtons/EditButtons.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DangerButton from "@/Components/Button/DangerButton.vue";
+
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <DangerButton>Delete</DangerButton>
+</template>
--- resources/js/Components/MusicList/Picker/Picker.vue
+++ resources/js/Components/MusicList/Picker/Picker.vue
@@ -1,4 +1,5 @@
 <script setup>
+import EditButtons from "./EditButtons/EditButtons.vue";
 import axios from "axios";
 import { ref, onMounted, reactive } from 'vue';
 
@@ -15,11 +16,33 @@ const musicLists = reactive({
 
 onMounted(() => {
     axios.get(route('user.musiclist.show'))
-    .then((response) => ( musicLists.list = response ));
+    .then((response) => ( musicLists.list = response.data ));
 });
+
+/* selection */
+const selectedList = ref(null);
+
+const select = (id) => {
+    selectedList = id;
+    console.log(id);
+};
 
 </script>
 
 <template>
-    {{ musicLists.list }}
+    <div 
+        v-for="item in musicLists.list"
+        class="mb-4"
+    >
+        <EditButtons
+            class="float-right" 
+            :id="item.id"
+        />
+
+        <button
+            @click="select(item.id)"
+        >
+            {{ item.title }}
+        </button>
+    </div>
 </template>
