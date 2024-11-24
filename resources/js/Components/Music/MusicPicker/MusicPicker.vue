397ba7406e651f98aac2efe6b34e0096664827fb
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2024-11-24 23:12:43 +0200

    preparing playlist track list

---
+++ resources/js/Components/Music/MusicPicker/MusicPicker.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const props = defineProps({
+    trackListItems: {
+        type: Object,
+        required: true
+    }
+});
+
+</script>
+
+<template>
+    {{ props.trackListItems }}
+</template>
--- resources/js/Pages/Content/MusicList/Show/Show.vue
+++ resources/js/Pages/Content/MusicList/Show/Show.vue
@@ -1,5 +1,7 @@
 <script setup>
 import WithSidebarSearchLayout from '@/Layouts/WithSidebarSearchLayout.vue';
+import MusicPicker from "@/Components/Music/MusicPicker/MusicPicker.vue";
+
 //import HorizMusicCard from '@/Components/Music/HorizMusicCard.vue';
 //import DeleteMusicForm from '@/Pages/Content/Music/Destroy/DeleteMusicForm.vue';
 //import ShowPlayButton from '@/Components/MusicPlayer/Components/ShowPlayButton.vue';
@@ -22,7 +24,23 @@ const props = defineProps({
     <Head :title="props.musicListInfo.title" />
 
     <WithSidebarSearchLayout>
-        <section>
+        <section
+            class="bg-white p-4 shadow sm:rounded-lg sm:p-8 flex flex-wrap"
+        >
+            <h1>
+                {{ props.musicListInfo.title }}
+            </h1>
+        </section>
+
+        <br>
+
+        <section
+            class="bg-white p-4 shadow sm:rounded-lg sm:p-8 flex flex-wrap"
+        >
+            <MusicPicker :trackListItems="props.musicListTracks"/>
+        </section>
+
+
             <!--<div v-if="$page.props.auth.user && $page.props.auth.user.id == musicList.creator_id">
                 <Link
                     :href="route('content.musiclist.edit', {
@@ -30,13 +48,5 @@ const props = defineProps({
                     })"
                 >Edit</Link>
             </div>-->
-
-        </section>
-        
-        {{ props.musicListInfo }}
-
-
-        {{ props.musicListTracks }}
-
     </WithSidebarSearchLayout>
 </template>
